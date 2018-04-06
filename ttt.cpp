d2e53f894e80027f861b407dd85ce587d25d7d19
Timothy Milligan <timothy_s_milligan@mcpsmd.org>
2018-04-06 13:55:48 +0000

    Added win/loss check functions

--- ttt.cpp
+++ ttt.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 
-void printBoard(int board[3][3]);
+void printBoard(int board[3][3], bool player1Turn);
+bool switchPlayer(bool playerTurn);
+int checkWin(int board[3][3]);
+int sumRow(int row[3]);
+int sumCol(int board[3][3], int column);
 
 int main() {
-	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	bool player1Turn = true;
 	
-	printBoard(board);
+	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int row, col;
+	
+	printBoard(board, player1Turn);
+	
 	
 	return 0;
 }
 
-void printBoard(int board[3][3]) {
+void printBoard(int board[3][3], bool player1Turn) {
+	if(player1Turn) {
+		std::cout << "Player 1's Turn" << std::endl;
+	} else {
+		std::cout << "Player 2's Turn" << std::endl;
+	}
 	for(int i = 0; i < 3; i++) {
 		for(int j = 0; j < 3; j++) {
 			switch(board[i][j]) {
@@ -27,4 +40,43 @@ void printBoard(int board[3][3]) {
 		}
 		std::cout << std::endl;
 	}
+}
+
+bool switchPlayer(bool playerTurn) {
+	return !playerTurn;
+}
+
+int checkWin(int board[3][3]) {
+	int result = 0;
+	
+	
+	//Check rows and cols for wins
+	for(int i = 0; i < 3; i++) {
+		int rowSum = sumRow(board[i]), colSum = sumCol(board, i);
+		if(rowSum > 2 || colSum > 2) {
+			result = 1;
+		} else if(rowSum < -2 || colSum < -2) {
+			result = -1;
+		}
+	}
+	
+	//Check diagonals for wins
+	int diagTop = board[0][0] + board[1][1] + board[2][2],
+		diagBot = board[2][0] + board[1][1] + board[0][2];
+			
+	if(diagTop > 2 || diagBot > 2) {
+		result = 1;
+	} else if(diagTop < -2 || diagBot < -2) {
+		result = -1;
+	}
+	
+	return result;
+}
+
+int sumRow(int row[3]) {
+	return row[0] + row[1] + row[2];
+}
+
+int sumCol(int board[3][3], int column) {
+	return board[0][column] + board[1][column] + board[2][column];
 }
