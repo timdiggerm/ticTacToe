1121a52c773ea16d3d3cd522b03022a88eed73d7
Nathan Hoernlein <hoernnathan@gmail.com>
2018-04-11 16:08:25 +0000

    Increased board size

--- ttt.cpp
+++ ttt.cpp
@@ -4,24 +4,18 @@ void printBoard(int board[3][3], bool player1Turn);
 int checkWin(int board[3][3]);
 int sumRow(int row[3]);
 int sumCol(int board[3][3], int column);
-
 int main() {
 	bool player1Turn = true;
-	
-
 	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 	int row, col, result = 0;
-	
 	while(checkWin(board) == 0) {
 		printBoard(board, player1Turn);
 		std::cout << "Enter a row and a column: ";
 		std::cin >> row >> col;
-		
 		while(board[row-1][col-1] != 0) {
 			std::cout <<"Enter a valid, empty row and column choice: ";
 			std::cin >> row >> col;
 		}
-		
 		board[row-1][col-1] = player1Turn ? 1 : -1;
 		//Switch players
 		player1Turn = !player1Turn;
@@ -29,14 +23,12 @@ int main() {
 	printBoard(board, player1Turn);
 	return 0;
 }
-
 void printBoard(int board[3][3], bool player1Turn) {
 	if(player1Turn) {
 		std::cout << "Player 1's Turn" << std::endl;
 	} else {
 		std::cout << "Player 2's Turn" << std::endl;
 	}
-
 	std::cout << std::endl << "   ";
 	//Column headers
 	for(int i = 0; i < 3; i++) {
@@ -65,15 +57,12 @@ void printBoard(int board[3][3], bool player1Turn) {
 		std::cout << std::endl;
 		if(i != 2) {
 			//Print lines between rows
-			std::cout << "   ---+---+---" << std::endl;
+			std::cout << "   ----+----+----" << std::endl;
 		}
 	}
 }
-
 int checkWin(int board[3][3]) {
 	int result = 0;
-	
-	
 	//Check rows and cols for wins
 	for(int i = 0; i < 3; i++) {
 		int rowSum = sumRow(board[i]), colSum = sumCol(board, i);
@@ -83,24 +72,19 @@ int checkWin(int board[3][3]) {
 			result = -1;
 		}
 	}
-	
 	//Check diagonals for wins
 	int diagTop = board[0][0] + board[1][1] + board[2][2],
 		diagBot = board[2][0] + board[1][1] + board[0][2];
-			
 	if(diagTop > 2 || diagBot > 2) {
 		result = 1;
 	} else if(diagTop < -2 || diagBot < -2) {
 		result = -1;
 	}
-	
 	return result;
 }
-
 int sumRow(int row[3]) {
 	return row[0] + row[1] + row[2];
 }
-
 int sumCol(int board[3][3], int column) {
 	return board[0][column] + board[1][column] + board[2][column];
 }
