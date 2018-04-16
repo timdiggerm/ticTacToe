9844dfea956eea10a61e3b6ef15721bf4f2d5045
James Person <jpbean2000@gmail.com>
2018-04-16 16:01:30 +0000

    loops the game if you want, keeps track of score and displays at the end

--- ttt.cpp
+++ ttt.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <string>
 
 void printBoard(int board[3][3], bool player1Turn);
-
-int checkWin(int board[3][3]);
+void clearBoard(int board[3][3]);
+int checkWin(int board[3][3], bool &cont);
 int sumRow(int row[3]);
 int sumCol(int board[3][3], int column);
 
+
 int main() {
 	bool player1Turn = true;
-	
-	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int player1score, player2score = 0;
 	int row, col, result = 0;
-
-	while(checkWin(board) == 0) {
+	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	bool loep = false;
+	while(loep == false) {
+		if(checkWin(board, loep) == 1){
+			player1score++;
+			clearBoard(board);
+		}else if(checkWin(board, loep) == -1){
+			player2score++;
+			clearBoard(board);
+		}
+		
 		printBoard(board, player1Turn);
 		std::cout << "Enter a row and a column: ";
 		std::cin >> row >> col;
@@ -26,9 +36,9 @@ int main() {
 		//Switch players
 		player1Turn = !player1Turn;
 	}
-	printBoard(board, player1Turn);
-
-
+	
+	std::cout << std::endl << "Final Score: " << std::endl << "Player 1: " << player1score 
+	<< std::endl << "Player 2: " << player2score << std::endl;
 	
 	return 0;
 }
@@ -36,9 +46,9 @@ int main() {
 
 void printBoard(int board[3][3], bool player1Turn) {
 	if(player1Turn) {
-		std::cout << "Player 1's Turn" << std::endl;
+		std::cout << "Player 1's Turn";
 	} else {
-		std::cout << "Player 2's Turn" << std::endl;
+		std::cout << "Player 2's Turn";
 	}
 
 	std::cout << std::endl << "   ";
@@ -75,9 +85,9 @@ void printBoard(int board[3][3], bool player1Turn) {
 	}
 }
 
-int checkWin(int board[3][3]) {
+int checkWin(int board[3][3], bool &cont) {
 	int result = 0;
-	
+	char strang;
 	
 	//Check rows and cols for wins
 	for(int i = 0; i < 3; i++) {
@@ -95,18 +105,30 @@ int checkWin(int board[3][3]) {
 			
 	if(diagTop > 2 || diagBot > 2) {
 		result = 1;
+
 	} else if(diagTop < -2 || diagBot < -2) {
 		result = -1;
 	}
-	
-	//Print winner
-	if(result == 1){
-		std::cout << "Player 1 Wins!";
-	}else if(result == -1){
-		std::cout << "Player 2 Wins!";
+	if(result != 0){
+		printBoard(board, false);
+		std::cout << "Would you like to play again? (Y/N)" << std::endl;
+		std::cin >> strang;
+		if(strang=='y'){
+			std::cout << "Resetting..." << std::endl;
+		}else{
+			cont = true;
+		}
 	}
-	
+
 	return result;
+}
+
+void clearBoard(int board[3][3]){
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; i++){
+			board[i][j] = 0;
+		}
+	}
 }
 
 int sumRow(int row[3]) {
