ca79777b8c0ed1486514c858c2050e5feb4b39dc
riyadeb <38138494+riyadeb@users.noreply.github.com>
2018-04-26 12:31:15 -0400

    customizeBoardSize

--- ttt.cpp
+++ ttt.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
-
-void printBoard(int board[3][3], bool player1Turn);
-int checkWin(int board[3][3]);
-int sumRow(int row[3]);
-int sumCol(int board[3][3], int column);
+int rowCount = 3;
+int colCount = 3;
+int diagTop;
+int diagBot;
+void boardSize();
+void printBoard(int** board, bool player1Turn);
+int checkWin(int** board);
+int sumRow(int* board);
+int sumCol(int** board, int column);
 
 
 int main() {
+	using namespace std;
 	bool player1Turn = true;
 	
-
-	int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 	int row, col, result = 0;
-
+	boardSize();
+	int** board = new int*[rowCount]; //initializes a board to create dimensions with desired row & columns
+	for(int i = 0; i < rowCount; ++i)
+		board[i] = new int[colCount];
 	
 	while(checkWin(board) == 0) {
 		printBoard(board, player1Turn);
@@ -32,8 +38,15 @@ int main() {
 	return 0;
 }
 
+void boardSize(){
+	std::cout << "How big would you like your board to be? Enter row and column:";
+	int rowNew, columnNew;
+	std::cin >> rowNew >> columnNew;
+	rowCount = rowNew;
+	colCount = columnNew;
+}
 
-void printBoard(int board[3][3], bool player1Turn) {
+void printBoard(int** board, bool player1Turn) {
 	if(player1Turn) {
 		std::cout << "Player 1's Turn" << std::endl;
 	} else {
@@ -42,14 +55,14 @@ void printBoard(int board[3][3], bool player1Turn) {
 
 	std::cout << std::endl << "   ";
 	//Column headers
-	for(int i = 0; i < 3; i++) {
+	for(int i = 0; i < colCount; i++) {
 		std::cout << " " << i+1 << "  ";
 	}
 	std::cout << std::endl;
 
-	for(int i = 0; i < 3; i++) {
+	for(int i = 0; i < rowCount; i++) {
 		std:: cout << " " << i+1 << " "; //Row headers
-		for(int j = 0; j < 3; j++) {
+		for(int j = 0; j < colCount; j++) {
 			//Board uses 0 for unused, 1 for X and -1 for O
 			switch(board[i][j]) {
 				case 0:
@@ -62,50 +75,64 @@ void printBoard(int board[3][3], bool player1Turn) {
 					std::cout << " O ";
 					break;
 			}
-			if(j != 2) {
+			if(j != colCount) {
 				std::cout << "|"; //Lines between the columns
 			}
 		}
 		std::cout << std::endl;
-		if(i != 2) {
+		if(i != rowCount) {
 			//Print lines between rows
-			std::cout << "   ---+---+---" << std::endl;
+			std::cout << " -" << std::endl;
 		}
 	}
 }
 
 
-int checkWin(int board[3][3]) {
+int checkWin(int** board) {
 	int result = 0;
 	
 	
 	//Check rows and cols for wins
-	for(int i = 0; i < 3; i++) {
+	for(int i = 0; i < rowCount; i++) {
 		int rowSum = sumRow(board[i]), colSum = sumCol(board, i);
-		if(rowSum > 2 || colSum > 2) {
+		if(rowSum > rowCount-1 || colSum > colCount - 1) {
 			result = 1;
-		} else if(rowSum < -2 || colSum < -2) {
+		} else if(rowSum < 0-(rowCount-1) || colSum < 0-(colCount-1)) {
 			result = -1;
 		}
 	}
 	
 	//Check diagonals for wins
-	int diagTop = board[0][0] + board[1][1] + board[2][2],
-		diagBot = board[2][0] + board[1][1] + board[0][2];
+	for(int i = board[0][0]; i < board[rowCount -2][colCount-1]; i++){
+		diagTop = board[i][i];
+	}
+	
+	for(int i = board[0][colCount-1]; i < board[rowCount-1][0]; i--){
+		diagBot = board[i][i];
+	}
+	
+
 			
-	if(diagTop > 2 || diagBot > 2) {
+	if(diagTop > rowCount-1 || diagBot > rowCount-1) {
 		result = 1;
-	} else if(diagTop < -2 || diagBot < -2) {
+	} else if(diagTop < 0-(rowCount-1) || diagBot < 0-(rowCount-1)) {
 		result = -1;
 	}
 	
 	return result;
 }
 
-int sumRow(int row[3]) {
-	return row[0] + row[1] + row[2];
+int sumRow(int* row) {
+	for (int i =0 ; i < rowCount; i ++){
+			return row[i];
+	}
+
 }
 
-int sumCol(int board[3][3], int column) {
-	return board[0][column] + board[1][column] + board[2][column];
+int sumCol(int** board, int column) {
+	for(int i =0 ; i < colCount; i++){
+		return board[i][column];
+	}
+	
 }
+
